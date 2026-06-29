4895288f8faa15ad6d00d6ba14ccdcb80976a249
Victor Reusch-Storgaard <Victor@r-s.nu>
2026-06-29 11:06:55 +0200

    mac install maybe

--- MyApp/obj/Release/net10.0/osx-arm64/MyApp.AssemblyInfo.cs
+++ MyApp/obj/Release/net10.0/osx-arm64/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47d515c86aae9e9ef98ab4feb93a02bb8c55e0f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92208444b310ba31922bdeb2e4d58cd93f403b8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
