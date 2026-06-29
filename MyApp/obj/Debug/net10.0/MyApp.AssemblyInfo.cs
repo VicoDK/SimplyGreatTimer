013aceef49be3685f96d2a56346c199fa1ddf41e
Vico <Victor@r-s.nu>
2026-06-29 13:26:49 +0200

    installs

--- MyApp/obj/Debug/net10.0/MyApp.AssemblyInfo.cs
+++ MyApp/obj/Debug/net10.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4895288f8faa15ad6d00d6ba14ccdcb80976a249")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35957246322e3ca5065ece78105acf60cd977e90")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
